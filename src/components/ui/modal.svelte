18ea7a3af2a94d87d70761c719ea4044876aa639
namesfang <jixiang.f@gmail.com>
2024-12-26 18:38:31 +0800

    fix:modal组件点击遮罩关闭

--- src/components/ui/modal.svelte
+++ src/components/ui/modal.svelte
@@ -111,7 +111,7 @@
 	}
 </script>
 
-<Overlay onclose={closeControl} bind:visible>
+<Overlay onclose={closeControl} {maskClosable} bind:visible>
 	<!-- svelte-ignore a11y_click_events_have_key_events -->
 	<!-- svelte-ignore a11y_no_static_element_interactions -->
 	<div onclick={(e)=> e.stopPropagation() } class="ui-modal" style={modalStyles}>
